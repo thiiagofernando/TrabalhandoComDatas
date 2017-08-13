95fed9fc4880bfd369ce2e6cd7f9186f02726e0a
Thiago Fernando <thiago.fernando@msn.com>
2017-08-13 16:27:35 -0400

    Datas Formatadas

--- TrabalhandoComDatas/TrabalhandoComDatas/Program.cs
+++ TrabalhandoComDatas/TrabalhandoComDatas/Program.cs
@@ -21,9 +21,18 @@ namespace TrabalhandoComDatas
             data = new DateTime(2017, 8, 13, 14,15,10,987);
             Console.WriteLine("#######");
             Console.WriteLine(data);
-            Console.WriteLine(data.ToString("HH:mm"));
-            Console.WriteLine(data.ToString("HH:mm:ss"));
+            Console.WriteLine(data.ToString("HH:mm"));// Hora e Minutos
+            Console.WriteLine(data.ToString("HH:mm:ss.fff"));//Data e Hora com Segundos
 
+            Console.WriteLine(data.ToString("D", new CultureInfo("pt-BR")));//Data Completa
+            Console.WriteLine(data.ToString("m", new CultureInfo("pt-BR")));//Dia numerico , Mes por extenso
+            Console.WriteLine(data.ToString("Y", new CultureInfo("pt-BR")));//Mes por extenso, ano numerico
+            Console.WriteLine(data.ToString("G", new CultureInfo("pt-BR")));//Data Geral
+            Console.WriteLine(data.ToString("g", new CultureInfo("pt-BR")));//Data Geral sem segundos
+            Console.WriteLine(data.ToString("O", new CultureInfo("pt-BR")));//Formato de Ida e Volta
+            Console.WriteLine(DateTime.Parse(data.ToString("O")).ToString("dd/MM/yyy HH:mm:ss.fff"));//Formato de Ida e Volta Formatado
+            Console.WriteLine(data.ToString("t"));//Formato de tempo
+            Console.WriteLine(data.ToString("T"));//Formato de tempo
             Console.ReadKey();
         }
     }
