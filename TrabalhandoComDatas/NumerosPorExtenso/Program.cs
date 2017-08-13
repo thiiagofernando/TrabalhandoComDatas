31e569da36a251a74795517257d7bdf707331b76
Thiago Fernando <thiago.fernando@msn.com>
2017-08-13 18:38:01 -0400

    Trabalhando com Numero e Moedas BRL

---
+++ TrabalhandoComDatas/NumerosPorExtenso/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caelum.Stella.CSharp.Inwords;
+
+namespace NumerosPorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 75;
+            string extenso = new Numero(valor).Extenso();
+            Console.WriteLine(extenso);
+
+            valor = 2567;
+            extenso=new Numero(valor).Extenso();
+            Console.WriteLine(extenso);
+
+            double valor2 = 2567.98;
+            string extensobrl= new MoedaBRL(valor2).Extenso();
+            Console.WriteLine(extensobrl);
+
+
+            Console.ReadKey();
+        }
+    }
+}
